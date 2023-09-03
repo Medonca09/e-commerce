0eea991c55f96340830a498753049297c5b9d70f
Medonca09 <lucasmm0908@gmail.com>
2023-09-03 13:09:22 -0300

    Criação de checkout para e-commerce | Parte 2

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,5 +1,10 @@
 import { catalogo } from "./utilidades";
 
+const idsProdutoCarrinhoComQuantidade = {
+
+
+}
+
 function abrirCarrinho() {
     document.getElementById("carrinho").classList.add("right-[0px]");
     document.getElementById("carrinho").classList.remove("right-[-360px]");
@@ -18,19 +23,97 @@ export function inicializarCarrinho() {
     botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
 }
 
-export function adicionarAoCarrinho(idProduto) {
-    const produto = catalogo.find(p => p.id === idProduto);
-    const containerProdutosCarrinho = document.getElementById('produtos-carrinho')
-    const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
-    <button id="fechar-carrinho" class="absolute top-0 right-2">
+function removerDoCarrinho(idProduto){
+  delete idsProdutoCarrinhoComQuantidade[idProduto];
+  renderizarProdutoCarrinho();
+}
+
+function incrementarQuantidadeProduto(idProduto){
+  idsProdutoCarrinhoComQuantidade[idProduto]++;
+  atualizarInformacaoQuantidade(idProduto);
+}
+
+function decrementarQuantidadeProduto(idProduto){
+  if(idsProdutoCarrinhoComQuantidade[idProduto] === 1){
+    removerDoCarrinho(idProduto);
+    return;
+  }
+  idsProdutoCarrinhoComQuantidade[idProduto]--;
+  atualizarInformacaoQuantidade(idProduto);
+}
+
+function atualizarInformacaoQuantidade(idProduto){
+  document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
+}
+
+function desenharProdutoNoCarrinho(idProduto){
+  const produto = catalogo.find(p => p.id === idProduto);
+
+    const containerProdutosCarrinho = 
+    document.getElementById('produtos-carrinho');
+
+    const elementoArticle = document.createElement('article');
+
+    const articleClasses = [
+     'flex',
+     'bg-slate-100', 
+     'rounded-lg', 
+     'p-1', 
+     'relative'
+    ];
+
+    for(const articleClass of articleClasses){
+      elementoArticle.classList.add(articleClass)
+    } 
+
+    const cartaoProdutoCarrinho = `<button id="remover-item-${produto.id}" class="absolute top-0 right-2">
       <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-red-700"></i>
     </button>
     <img src="./assets/img/${produto.imagem}" alt="${produto.nome}" class="h-24 rounded-lg"/>
-    <div class="py-2">
+    <div class="p-2 flex flex-col justify-between">
       <P class="text-slate-900 text-xs">${produto.nome}</P>
       <p class="text-slate-400">Tamanho: M</p>
       <p class="text-green-700 text-lg">$${produto.preco}</p>
     </div>
-  </article>`
-  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+    <div class='flex text-slate-950 items-end absolute bottom-0 right-2'>
+        <button id='decrementar-produto-${produto.id}'>-</button>
+        <p id='quantidade-${produto.id}' class='ml-2'>${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+        <button class='ml-2' id='incrementar-produto-${produto.id}'>+</button>
+    </div>`
+
+  elementoArticle.innerHTML = cartaoProdutoCarrinho;
+  containerProdutosCarrinho.appendChild(elementoArticle);
+
+  document
+  .getElementById(`decrementar-produto-${produto.id}`)
+  .addEventListener('click', () => decrementarQuantidadeProduto(produto.id));
+  
+  document
+  .getElementById(`incrementar-produto-${produto.id}`)
+  .addEventListener('click', () => incrementarQuantidadeProduto(produto.id));
+  
+  document
+  .getElementById(`remover-item-${produto.id}`)
+  .addEventListener('click', () => removerDoCarrinho(produto.id));
+}
+
+function renderizarProdutoCarrinho(){
+  const containerProdutosCarrinho = 
+    document.getElementById('produtos-carrinho');
+    containerProdutosCarrinho.innerHTML = '';
+
+    for(const idProduto in idsProdutoCarrinhoComQuantidade){
+      desenharProdutoNoCarrinho(idProduto);
+    }
+    
+}
+
+export function adicionarAoCarrinho(idProduto) {
+    if(idProduto in idsProdutoCarrinhoComQuantidade){
+      incrementarQuantidadeProduto(idProduto);
+      return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+    desenharProdutoNoCarrinho(idProduto);
+    
 }
--- src/utilidades.js
+++ src/utilidades.js
@@ -1,6 +1,6 @@
 export const catalogo = [
     {
-      id: 1,
+      id: "1",
       marca: "Zara",
       nome: "Camisa Larga com Bolsos",
       preco: 70,
@@ -8,7 +8,7 @@ export const catalogo = [
       feminino: false,
     },
     {
-      id: 2,
+      id: "2",
       marca: "Zara",
       nome: "Casaco Reto com Lã",
       preco: 85,
@@ -16,7 +16,7 @@ export const catalogo = [
       feminino: true,
     },
     {
-      id: 3,
+      id: "3",
       marca: "Zara",
       nome: "Jaqueta com Efeito Camurça",
       preco: 60,
@@ -24,7 +24,7 @@ export const catalogo = [
       feminino: false,
     },
     {
-      id: 4,
+      id: "4",
       marca: "Zara",
       nome: "Sobretudo em Mescla de Lã",
       preco: 160,
@@ -32,7 +32,7 @@ export const catalogo = [
       feminino: false,
     },
     {
-      id: 5,
+      id: "5",
       marca: "Zara",
       nome: "Camisa Larga Acolchoada de Veludo Cotelê",
       preco: 110,
@@ -40,7 +40,7 @@ export const catalogo = [
       feminino: false,
     },
     {
-      id: 6,
+      id: "6",
       marca: "Zara",
       nome: "Casaco de Lã com Botões",
       preco: 170,
@@ -48,7 +48,7 @@ export const catalogo = [
       feminino: true,
     },
     {
-      id: 7,
+      id: "7",
       marca: "Zara",
       nome: "Casaco com Botões",
       preco: 75,
@@ -56,7 +56,7 @@ export const catalogo = [
       feminino: true,
     },
     {
-      id: 8,
+      id: "8",
       marca: "Zara",
       nome: "Colete Comprido com Cinto",
       preco: 88,
