b2a5ad0427455a7bf63bcb6196229e5dc3bf8624
Medonca09 <lucasmm0908@gmail.com>
2024-01-07 17:56:48 -0300

    Trazendo a listagem dos produtos por categoria

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
+import { iniciarlizarFiltros } from "./src/filtrosCatalogos";
 import { atualizarPrecoCarrinho, inicializarCarrinho, renderizarProdutoCarrinho } from "./src/menuCarrinho";
 
 inicializarCarrinho();
 renderizarCatalogo();
 atualizarPrecoCarrinho();
 renderizarProdutoCarrinho();
+iniciarlizarFiltros();
+iniciarlizarFiltros;
